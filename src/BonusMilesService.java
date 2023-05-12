92b6ce740ff94a88e6826ae1e7964f5ac9896665
Vladko1973 <vpk1973@yandex.ru>
2023-05-12 14:14:56 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+package org.example;
+public class BonusMilesService {
+    public int calculate (int ticketPrice) {
+        int costOfFrequentFlyerMile = 20;
+        int bonus;
+        bonus = ticketPrice/costOfFrequentFlyerMile;
+        return bonus;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import org.example.BonusMilesService;
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService bonus = new BonusMilesService ();
+
+//Объявление переменных (стоимость билета и бонуса) и вывод на консоль
+        int ticketCost = 123345;
+        System.out.println("The cost of a ticket = " + ticketCost + " roubles");
+
+//Объявление переменной для вывода, расчет и вывод на консоль
+        int NumberOfFrequentFlyerMiles;
+        NumberOfFrequentFlyerMiles = bonus.calculate (ticketCost);
+            System.out.println("The bonus amount = " + NumberOfFrequentFlyerMiles + " free miles accrued");
+    }
+}
