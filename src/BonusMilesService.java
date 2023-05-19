b74d569a31c0f5cf94df12d13ccf918011c34f3e
Vladimir Koretskiy <130839671+VladKoretski@users.noreply.github.com>
2023-05-19 23:22:24 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,10 @@
 package org.example;
+
 public class BonusMilesService {
-    public int calculate (int ticketPrice) {
+    public int calculate(int ticketPrice) {
         int costOfFrequentFlyerMile = 20;
         int bonus;
-        bonus = ticketPrice/costOfFrequentFlyerMile;
+        bonus = ticketPrice / costOfFrequentFlyerMile;
         return bonus;
     }
 }
